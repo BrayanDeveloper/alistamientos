09f0bf6501c4ed4c0e5ca4a323137b9ddaafad3d
Brayan <developerbrayan@gmail.com>
2019-07-20 09:50:19 -0500

    subida de iconos de la barra de navegacion del panel

--- inc/nav/navegacionPlataforma.php
+++ inc/nav/navegacionPlataforma.php
@@ -6,25 +6,50 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-      <a class="navbar-brand" href="#"><i class="fa fa-plus-square"></i> Nuevo</a>
+      
     </div>
+
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="">
-            <a href="crear-alistamiento"><i class="fa fa-align-left"></i> Nuevo Alistamiento</a>
-        </li>
-        <li class="">
-            <a href="ver-alistamientos"><i class="fa fa-circle"></i> Ver Alistamientos</a>
-        </li>
-        <li class="">
-            <a href="subir-firma"><i class="fa fa-circle"></i> Subir Firma</a>
-        </li>
+
+          <ul class="nav navbar-nav">
+            <li class="">
+              <a href="subir-firma"><img src="img/iconos/www.png"> Subir Firma</a>
+            </li>
+         
+          
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#"><img src="img/iconos/test.png"> Alistamientos
+              <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="ver-alistamientos">Ver Alistamientos</a></li>
+                <li><a href="crear-alistamiento">Nuevo Alistamiento</a></li>
+            </li>
+            
+        </ul>
+        
+          <!-- <li class="">
+              <a href="ver-alistamientos"><i class="fa fa-circle"></i> Ver Alistamientos</a>
+          </li> -->
+        
         <li>
             
         </li>
         <!-- <li><a href="#">Page 2</a></li> 
         <li><a href="#">Page 3</a></li>  -->
       </ul>
+      <ul class="nav navbar-nav">
+           
+          
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#"><img src="img/iconos/question.png"> Soporte 
+              <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="ver-tiket">Ver Tikets</a></li>
+                <li><a href="crear-tiket">Nuevo Tiket</a></li>
+            </li>
+            
+        </ul>
+      
       <ul class="nav navbar-nav navbar-right">
         <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
@@ -32,5 +57,8 @@
     </div>
   </div>
 </nav>
+
+
+
 
 
